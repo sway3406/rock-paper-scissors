d5b61145fc674b4aa7c6438892896b5d1f0fcd42
Shane Way <shane.c.way2@gmail.com>
2022-06-12 15:41:38 -0600

    Add class and constructor for Player. No methods added yet

---
+++ src/Player.js
@@ -0,0 +1,8 @@
+class Player {
+  constructor(playerObj) {
+    this.name = playerObj.name;
+    this.image = playerObj.image;
+    this.pickFighter = playerObj.pickFighter;
+    this.playerScore = 0;
+  }
+}
