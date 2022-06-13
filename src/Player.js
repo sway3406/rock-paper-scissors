d32ef48bded66a7fb4d8f2c162e6aabdab0462e7
Shane Way <shane.c.way2@gmail.com>
2022-06-13 13:16:39 -0600

    This commit adds working method (assignFighterToHuman()), for human to select a fighter

--- src/Player.js
+++ src/Player.js
@@ -3,18 +3,18 @@ class Player {
     this.name = playerObj.name;
     this.pickFighter = null;
     this.playerScore = 0;
-    this.classics = ['rock', 'paper', 'scissors'];
+    this.classics = ['rock', 'paper', 'scissor'];
     this.flavorfuls = ['cake', 'pie', 'ice cream', 'cupcake', 'cookie'];
   }
   randomComputerFighter(gameType){
     this.pickFighter =  this[gameType][Math.floor(Math.random() * this[gameType].length)];
-    console.log('this.pickFighter', this.pickFighter);
+    console.log('computer picked:', this.pickFighter);
     console.log('gameType', gameType);
   }
   updatePlayerFighter(event) {
-  if (event.target.classList.contains('.fighter-buttons')) {
+    console.log(event.target.classList.contains('fighter-buttons'))
+  // if (event.target.classList.contains('fighter-buttons')) {
   this.pickFighter = event.target.id;
-  console.log('player choice', this.pickFighter)
+  console.log('human picked:', this.pickFighter)
   }
-}
 }
