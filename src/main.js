189f7b2613c5c0f06abe30a6ac5482f45639104f
Shane Way <shane.c.way2@gmail.com>
2022-06-14 09:28:14 -0600

    Add showResults function for displaying section in DOM. Only toggles currently. Not fully functioning

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 // Query Selectors
+var gameHeader = document.querySelector('.game-header');
 var chooseGameContainer =document.querySelector('.choose-game-container');
 var classicGame = document.querySelector('#classic-game-box');
 var flavorfulGame = document.querySelector('#flavorful-game-box');
 var classicContainer = document.querySelector('.classic-game-container');
 var flavorfulContainer =document.querySelector('.flavorful-game-container');
-
+var resultsContainer = document.querySelector('.results-container');
 
 //Buttons
 var changeGameButton = document.querySelector('#change-game-button');
 var resetGameButton = document.querySelector('#reset-game-button');
 var fighterButtons = document.querySelector('.main-stage');
-var gameHeader = document.querySelector('.game-header');
+
 
 //Event Handlers
 window.addEventListener('load', createPlayers);
@@ -31,8 +32,6 @@ function createPlayers() {
   computer = new Player({name: 'Computer'});
 }
 
-// This function will:
-// Change based on user input (if/else if)?
 function chooseGameHeader() {
  gameHeader.innerHTML = `<h2>Choose A Game</h2>`
 }
@@ -54,9 +53,33 @@ function showFlavorGame() {
   flavorfulContainer.classList.remove('hidden');
 }
 
+//This function will:
+// Get Invoked when a user selects an element by id from the DOM. This will either be from the classicVersion or the flavorfulVersion
+// This function will
+// hide the classicVersion
+// hide the flavorfulVersion
+// show the results Container
+
+function showResults(gameType) {
+    classicContainer.classList.add('hidden');
+    flavorfulContainer.classList.add('hidden');
+    resultsContainer.classList.remove('hidden');
+}
+
+// function showResults(gameType) {
+//   if (gameType === 'classics') {
+//     classicContainer.classList.add('hidden');
+//     resultsContainer.classList.remove('hidden');
+//   } else {
+//     flavorfulContainer.classList.add('hidden');
+//     resultsContainer.classList.remove('hidden');
+//   }
+// }
+
 function showGamesOptions() {
   classicContainer.classList.add('hidden');
   flavorfulContainer.classList.add('hidden');
+  resultsContainer.classList.add('hidden');
   chooseGameContainer.classList.remove('hidden');
 }
 
@@ -75,22 +98,15 @@ function assignFighters(event) {
  function pickWinner(gameType) {
    if (gameType === 'classics') {
     game.getClassicWinner();
+    showResults();
  } else {
    game.getFlavorfulWinner();
+   showResults();
  }
 }
 
 
-//This function will:
-// Get Invoked when a user selects an element by id from the DOM. This will either be from the classicVersion or the flavorfulVersion
-// This function will
-// hide the classicVersion
-// hide the flavorfulVersion
-// show the results Container
 
-// function showResults() {
-//
-// }
 
 
 
